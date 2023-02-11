7db1fdc8ed2d0627f2b96ebea1b67a88f640d980
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-02-11 12:11:16 -0300

    Update 5.CPF.js

--- 5.CPF.js
+++ 5.CPF.js
@@ -1,7 +1,43 @@
 class ValidaCPF {
     constructor(cpfEnviado){
-        Object.defineProperties(this, 'cpf')
+        Object.defineProperty(this, 'cpfLimpo',{
+            writable: false,
+            enumerable: false,
+            configurable: false,
+            value: cpfEnviado.replace(/|D+/g, '')
+        });
+    }
+
+    ehSequencia() {
+        return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length) === this.cpfLimpo;
+    }
+
+    geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = this.geraDigito(cpfSemDigitos);
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1)
+    }
+
+    geraDigito(cpfSemDigitos) {
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for(let stringNumerica of cpfSemDigitos) {
+            console.log(stringNumerica, typeof stringNumerica);
+        }
+    }
+
+    valida() {
+        if(!this.cpfLimpo) return false
+        if(typeof cpfLimpo !== 'string') return false;
+        if(this.cpfLimpo.length !== 11) return false;
+        if(this.ehSequencia()) return false;
+        if(! this.geraNovoCpf()) return false;
+
+
+        return 'Cheguei aqui';
     }
 }
 
 const validaCPF = new ValidaCPF('070.987.720-03');
+// console.log(validaCPF)
