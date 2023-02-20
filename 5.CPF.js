4051229058544b941a98f13740e192b119d6a182
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-02-20 14:51:14 -0300

    Form

--- 5.CPF.js
+++ 5.CPF.js
@@ -4,40 +4,51 @@ class ValidaCPF {
             writable: false,
             enumerable: false,
             configurable: false,
-            value: cpfEnviado.replace(/|D+/g, '')
+            value: cpfEnviado.replace(/\D+/g, '')
         });
     }
 
     ehSequencia() {
-        return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length) === this.cpfLimpo;
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
     }
 
     geraNovoCpf() {
         const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
-        const digito1 = this.geraDigito(cpfSemDigitos);
-        const digito2 = this.geraDigito(cpfSemDigitos + digito1)
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+        this.novoCPF = cpfSemDigitos + digito1 + digito2;
     }
 
-    geraDigito(cpfSemDigitos) {
+    static geraDigito(cpfSemDigitos) {
         let total = 0;
         let reverso = cpfSemDigitos.length + 1;
 
         for(let stringNumerica of cpfSemDigitos) {
-            console.log(stringNumerica, typeof stringNumerica);
+            total += reverso * Number(stringNumerica);
+            reverso--;
         }
+
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? String(digito) : '0';
     }
 
     valida() {
-        if(!this.cpfLimpo) return false
+        if(!this.cpfLimpo) return false;
         if(typeof cpfLimpo !== 'string') return false;
         if(this.cpfLimpo.length !== 11) return false;
         if(this.ehSequencia()) return false;
-        if(! this.geraNovoCpf()) return false;
+        this.geraNovoCPF();
 
-
-        return 'Cheguei aqui';
+        return this.novoCPF === this.cpfLimpo;
     }
 }
 
-const validaCPF = new ValidaCPF('070.987.720-03');
-// console.log(validaCPF)
+// let validacpf = new ValidaCPF('443.187.618-94');
+let validacpf = new ValidaCPF('070.987.720-03');
+// validacpf = new ValidaCPF('999.999.999-99');
+
+if (validacpf.valida()) {
+    console.log("CPF válido")
+} else {
+    console.log("CPF inválido");
+}
