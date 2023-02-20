398363d525442228c560b518e0cd188926c34ab9
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-02-20 19:36:35 -0300

    concluído

--- Form/assets/js/main.js
+++ Form/assets/js/main.js
@@ -13,6 +13,32 @@ class ValidaFormulario {
     handleSubmit(e) {
         e.preventDefault()
         const camposValidos = this.camposValidos();
+        const senhasValidas = this.senhasSaoValidas();
+
+        if(camposValidos && senhasValidas) {
+            alert('Formulário enviado.')
+            this.formulario.submit();
+        }
+    }
+
+    senhasSaoValidas() {
+        let valid = true;
+
+        const senha = this.formulario.querySelector(".senha");
+        const repetirSenha = this.formulario.querySelector(".repetir-senha");
+
+        if(senha.value !== repetirSenha.value) {
+            valid = false;
+            this.criaErro(senha,'Campos senha e repetir senha precisam ser iguais.')
+            this.criaErro(repetirSenha,'Campos senha e repetir senha precisam ser iguais.')
+        }
+
+        if(senha.value.length < 6 || senha.value.length > 12) {
+            valida = false;
+            this.criaErro(senha, 'Senha precisa ter entre 6 e 12 caracteres.');
+        }
+
+        return valid;
     }
 
     camposValidos() {
@@ -29,7 +55,45 @@ class ValidaFormulario {
                 this.criaErro(campo, `Campo ${label} não pode estar em branco`)
                 valid = false
             }
+
+            if(campo.classList.contains('cpf')) {
+                if(!this.validaCPF(campo)) valid = false;
+            }
+
+            if(campo.classList.contains('usuario')) {
+                if(!this.validaUsusario(campo)) valid = false;
+            }
         }
+
+        return valid;
+    }
+
+    validaUsuario(campo) {
+        const usuario = campo.value;
+        let valid = true;
+
+        if(usuario.length < 3 || usuario.length > 12){
+            this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres');
+            valid = false;
+        }
+
+        if(!usuario.match(/^[a-zA-Z0-9]+$/g)){
+            this.criaErro(campo, 'Nome de usuário deve conter apenas letras ou números');
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    validaCPF(campo) {
+        const cpf = new ValidaCPF(campo.value);
+
+        if(!cpf.valida()) {
+            this.criaErro(campo, 'CPF inválido');
+            return false;
+        }
+        
+        return true;
     }
 
     criaErro(campo, msg) {
